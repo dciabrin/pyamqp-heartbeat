3c28bea0e2c8cd22e585f0c0aad5c75916975213
Hervé Beraud <hberaud@redhat.com>
2019-06-05 17:50:39 +0200

    Allow server to run under a monkey patched environment if needed
    Adding parameters to allow user to monkey patch the environment before
    running the server and so run it inside.

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import argparse
+
+import eventlet
 
 from oslo_config import cfg
 import oslo_messaging
@@ -12,13 +15,30 @@ class TestEndpoint(object):
         return arg+1
 
 
-transport_url = 'rabbit://testuser:testpwd@localhost:5672/'
-transport = oslo_messaging.get_rpc_transport(cfg.CONF)
-target = oslo_messaging.Target(topic='test', server='myname')
-endpoints = [TestEndpoint()]
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--eventlet-turned-on', action='store_true',
+                        help='turn on eventlet and monkey patch the env')
+    parser.add_argument("--heartbeat-timeout", help="the heartbeat timeout",
+                        default=60)
+    args = parser.parse_args()
+    if args.eventlet_turned_on:
+        print("----------------------------------------------")
+        print("/!\  Running a monkey patched environment  /!\\")
+        print("----------------------------------------------")
+        eventlet.monkey_patch()
 
-server = oslo_messaging.get_rpc_server(transport, target, endpoints,
-                                       executor='threading')
-server.start()
-while True:
-    time.sleep(1)
+    transport_url = 'rabbit://testuser:testpwd@localhost:5672/'
+    transport = oslo_messaging.get_rpc_transport(cfg.CONF)
+    target = oslo_messaging.Target(topic='test', server='myname')
+    endpoints = [TestEndpoint()]
+    
+    server = oslo_messaging.get_rpc_server(transport, target, endpoints,
+                                           executor='threading')
+    server.start()
+    while True:
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
